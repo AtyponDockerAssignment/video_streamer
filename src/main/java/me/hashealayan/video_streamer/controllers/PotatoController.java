77231f9538e3ffac5431e279b3079ef49f1614a8
hashem78 <hashem.olayano@gmail.com>
2023-08-17 02:11:06 +0300

    Fix redirect

--- src/main/java/me/hashealayan/video_streamer/controllers/PotatoController.java
+++ src/main/java/me/hashealayan/video_streamer/controllers/PotatoController.java
@@ -1,6 +1,7 @@
 package me.hashealayan.video_streamer.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -9,5 +10,10 @@ public class PotatoController {
     @PostMapping(value = "/login")
     public String login() {
         return "redirect:blobs";
+    }
+
+    @GetMapping(value = "/blobs")
+    public String blobs() {
+        return "blobs";
     }
 }
